43630a85cf5cfaaa73a318c2fc82655980b63781
ezeiger92 <ezeiger92@gmail.com>
2020-01-14 20:17:01 -0500

    Potentially a plugin now

--- src/main/java/com/chromaclypse/territories/TerritoryMain.java
+++ src/main/java/com/chromaclypse/territories/TerritoryMain.java
@@ -2,11 +2,17 @@ package com.chromaclypse.territories;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.chromaclypse.territories.command.Commands;
+
 public class TerritoryMain extends JavaPlugin {
 	private Territory territory;
+	
 	@Override
 	public void onEnable() {
 		territory = new Territory(this);
+		Commands commandsMapping = new Commands(territory);
+		
+		commandsMapping.initialize();
 	}
 	
 	@Override
---
+++ src/main/java/com/chromaclypse/territories/command/Commands.java
@@ -0,0 +1,42 @@
+package com.chromaclypse.territories.command;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.bukkit.command.TabExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.chromaclypse.api.command.CommandBase;
+import com.chromaclypse.api.command.Context;
+import com.chromaclypse.territories.Territory;
+
+public class Commands {
+	private final Territory handle;
+	private final Manager managerCmds;
+	private final Resident residentCmds;
+	
+	public Commands(Territory handle) {
+		this.handle = handle;
+		managerCmds = new Manager(handle);
+		residentCmds = new Resident(handle);
+	}
+	
+	private List<String> allTerritories(Context context){
+		return handle.getClaims().stream().map(c -> c.name).collect(Collectors.toList());
+	}
+	
+	public void initialize() {
+		JavaPlugin plugin = (JavaPlugin) handle.getPlugin();
+		
+		TabExecutor command = new CommandBase()
+				.with().arg("spawn").calls(residentCmds::spawn)
+				.with().arg("spawn").option(this::allTerritories).calls(residentCmds::spawnOther)
+				.with().arg("create").calls(managerCmds::create)
+				.with().arg("move").calls(managerCmds::move)
+				.with().arg("version").calls(CommandBase::pluginVersion)
+				.getCommand();
+
+		plugin.getCommand("territory").setExecutor(command);
+		plugin.getCommand("territory").setTabCompleter(command);
+	}
+}
